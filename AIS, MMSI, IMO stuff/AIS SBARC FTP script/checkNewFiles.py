05238313e51b041682c3d7d927762428f5fd53f9
Alex Luu <alluu@ucsd.edu>
2017-06-26 11:06:09 -0700

    Staging deleted files

--- AIS, MMSI, IMO stuff/AIS SBARC FTP script/checkNewFiles.py
+++
@@ -1,7 +0,0 @@
-#just calls the check new files function
-#written this way because easier to be called in bash
-from everything import parseBoth
-from everything import checkForNewFiles
-
-parseBoth()
-checkForNewFiles()
